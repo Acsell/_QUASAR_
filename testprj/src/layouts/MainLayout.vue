3684d9f0e2d9a5d716c719370c42c2e02046c34b
Acsell <inginer2004@mail.ru>
2020-12-09 23:34:40 +0300

    Update MainLayout.vue

--- testprj/src/layouts/MainLayout.vue
+++ testprj/src/layouts/MainLayout.vue
@@ -41,8 +41,30 @@
     </q-drawer>
     <q-page-container>
       <nav>
-      <a href="#anchor1">JJJJJJ</a>
-      <a href="#anchor2">JJJJJJ</a>
+        <span id="scroll">
+
+      <a href="#anchor1" style="text-decoration: none;">
+        <div id="scroll1">
+          <div id="slider1"></div>
+          <div id="slider2"></div>
+          <p id="s1">Header</p>
+          </div>
+          </a>
+      <a href="#anchor2" style="text-decoration: none;">
+        <div id="scroll2">
+          <div id="slider1"></div>
+          <div id="slider2"></div>
+          <p id="s2">Content</p>
+          </div>
+          </a>
+      <a href="#anchor3" style="text-decoration: none;">
+        <div id="scroll3">
+          <div id="slider1"></div>
+          <div id="slider2"></div>
+          <p id="s3">Folder</p>
+          </div>
+          </a>
+        </span>
       </nav>
       <div id="cub">
         <div id="anchor1">
@@ -54,9 +76,11 @@
         <img id="img1" src="https://trafficlite.000webhostapp.com/images/traffic.gif" width="50%"/>
         </section>
         </div>
-
         <div id="anchor2">
-          <p id="text2">{{s2}}</p>
+          <p id="text2">{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}{{text}}</p>
+        </div>
+        <div id="anchor3">
+          <p id="text3">{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}{{text2}}</p>
         </div>
       </div>
     </q-page-container>
@@ -64,8 +88,100 @@
 </template>
 
 <style>
+  #scroll
+  {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
+  /*  */
+  #scroll1
+  {
+    margin-left:5%;
+    margin-top: 25%;
+    width:6%;
+    height: 3%;
+    color: black;
+    background: grey;
+  }
+  #scroll1:not(hover) div{
+    transition:1s;
+  } 
+  #scroll1:active div
+  {
+    transition:0.5s;
+    background:black;
+    transform: translate(80px, 80px);
+    transform: scale(1000) rotate(45rad);
+    opacity: 10;
+  } 
+  #slider1
+  {
+    position: absolute;
+    left: -5%;
+    top: 0%;
+    width: 1%;
+    height: 1%;
+    background: blue;
+    transform: rotate(45rad);
+    z-index: 1000;
+  }
+  #s1
+  {
+    font-size: 1vw;
+  } 
+  /*  */
+  #scroll2{
+    margin-left:5%;
+    width:5%;
+    height: 3%;
+    color: black;
+    margin-top: 1%;
+    background: grey;
+  }
+  #scroll2:not(hover) div{
+    transition:1s;
+  } 
+  #scroll2:active div
+  {
+    transition:0.5s;
+    background:black;
+    transform: translate(80px, 80px);
+    transform: scale(1000) rotate(45rad);
+    opacity: 10;
+  } 
+  #s2
+  {
+    font-size: 1vw;
+  }
+  #scroll3
+  {
+    margin-left:5%;
+    width:4%;
+    height: 3%;
+    min-height: 5px;
+    color: black;
+    margin-top: 1%;
+    background: grey;
+  }
+  #scroll3:not(hover) div{
+    transition:1s;
+  } 
+  #scroll3:active div
+  {
+    transition:0.1s;
+    background:black;
+    transform: translate(80px, 80px);
+    transform: scale(1000) rotate(45rad);
+    opacity: 10;
+  } 
+  #s3
+  {
+    font-size: 1vw;
+  }
   #cub
   {
+    z-index: -100;
     width: 50%;
     height: 90%;
     background: #fafafa;
@@ -97,10 +213,17 @@
   }
   #anchor2
   {
-    position: absolute;
-    margin-top: 50%;
+    margin-top: 10%;
+  }
+  #anchor3
+  {
+    margin-top: 10%;
   }
   #text2
+  {
+    font-size: 1vw;
+  }  
+  #text3
   {
     font-size: 1vw;
   }
@@ -142,8 +265,9 @@ export default defineComponent({
     const essentialLinks = ref(linksData);
     const s1 = ref("Светофоры с искусственным интеллектом, которые позволяют расформировать транспортный коллапс, в отличие от светофоров прошлого поколения.");
     const s2 = ref("Светофоры с искусственным интеллектом, которые позволяют расформировать транспортный коллапс, в отличие от светофоров прошлого поколения.");
-
-    return { leftDrawerOpen, essentialLinks, s1, s2 };
+    const text = ref("Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст");
+    const text2 = ref("Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2 Текст2");
+    return { leftDrawerOpen, essentialLinks, s1, s2, text , text2};
   },
 });
 </script>
